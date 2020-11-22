ccff48f6b0b8fb1e6732922dac38559f5c8cc0da
Vsevolod <sevkaplay@gmail.com>
2020-11-22 17:44:03 +0300

    Create the first three js scene

---
+++ dist/client/client.js
@@ -0,0 +1,18 @@
+import * as THREE from '/build/three.module.js';
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
+let scene = new THREE.Scene();
+scene.add(cube);
+camera.position.z = 2;
+var animate = function () {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+};
+animate();
