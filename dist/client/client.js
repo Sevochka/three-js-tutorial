6fdd9ff4b3c2c119f77a8fde182755806f9e3c8a
Vsevolod <sevkaplay@gmail.com>
2020-11-27 15:12:15 +0300

    Add shine thingy

--- dist/client/client.js
+++ dist/client/client.js
@@ -20,12 +20,12 @@ const sphereGeometry = new THREE.SphereGeometry();
 const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0);
 const planeGeometry = new THREE.PlaneGeometry();
 const torusKnotGeometry = new THREE.TorusKnotGeometry();
-const material = new THREE.MeshLambertMaterial();
+const material = new THREE.MeshPhongMaterial();
 // const texture = new THREE.TextureLoader().load("img/grid.png")
 // material.map = texture
 // const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
-// texture.mapping = THREE.CubeReflectionMapping
-// // //texture.mapping = THREE.CubeRefractionMapping
+// envTexture.mapping = THREE.CubeReflectionMapping
+// envTexture.mapping = THREE.CubeRefractionMapping
 // material.envMap = envTexture
 const cube = new THREE.Mesh(boxGeometry, material);
 cube.position.x = 5;
@@ -77,17 +77,20 @@ materialFolder.open();
 var data = {
     color: material.color.getHex(),
     emissive: material.emissive.getHex(),
+    specular: material.specular.getHex()
 };
-var meshLambertMaterialFolder = gui.addFolder('THREE.MeshLambertMaterial');
-meshLambertMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))); });
-meshLambertMaterialFolder.addColor(data, 'emissive').onChange(() => { material.emissive.setHex(Number(data.emissive.toString().replace('#', '0x'))); });
-meshLambertMaterialFolder.add(material, 'wireframe');
-meshLambertMaterialFolder.add(material, 'wireframeLinewidth', 0, 10);
-meshLambertMaterialFolder.add(material, 'flatShading').onChange(() => updateMaterial());
-meshLambertMaterialFolder.add(material, 'combine', options.combine).onChange(() => updateMaterial());
-meshLambertMaterialFolder.add(material, 'reflectivity', 0, 1);
-meshLambertMaterialFolder.add(material, 'refractionRatio', 0, 1);
-meshLambertMaterialFolder.open();
+var meshPhongMaterialFolder = gui.addFolder('THREE.MeshPhongMaterial');
+meshPhongMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))); });
+meshPhongMaterialFolder.addColor(data, 'emissive').onChange(() => { material.emissive.setHex(Number(data.emissive.toString().replace('#', '0x'))); });
+meshPhongMaterialFolder.addColor(data, 'specular').onChange(() => { material.specular.setHex(Number(data.specular.toString().replace('#', '0x'))); });
+meshPhongMaterialFolder.add(material, 'shininess', 0, 1024);
+meshPhongMaterialFolder.add(material, 'wireframe');
+meshPhongMaterialFolder.add(material, 'wireframeLinewidth', 0, 10);
+meshPhongMaterialFolder.add(material, 'flatShading').onChange(() => updateMaterial());
+meshPhongMaterialFolder.add(material, 'combine', options.combine).onChange(() => updateMaterial());
+meshPhongMaterialFolder.add(material, 'reflectivity', 0, 1);
+meshPhongMaterialFolder.add(material, 'refractionRatio', 0, 1);
+meshPhongMaterialFolder.open();
 function updateMaterial() {
     material.side = Number(material.side);
     material.combine = Number(material.combine);
