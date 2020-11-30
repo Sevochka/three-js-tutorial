986b933d97e77a6d275d725c76480483f514d1d0
Vsevolod <sevkaplay@gmail.com>
2020-11-30 17:02:20 +0300

    Try PointerLockControls - cool for games

--- dist/client/client.js
+++ dist/client/client.js
@@ -1,35 +1,71 @@
 import * as THREE from '/build/three.module.js';
-import { TrackballControls } from '/jsm/controls/TrackballControls';
+import { PointerLockControls } from '/jsm/controls/PointerLockControls';
 import Stats from '/jsm/libs/stats.module';
 const scene = new THREE.Scene();
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-var light = new THREE.HemisphereLight();
+var light = new THREE.AmbientLight();
 scene.add(light);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-const controls = new TrackballControls(camera, renderer.domElement);
-// controls.addEventListener('change', () => console.log("Controls Change"))
-// controls.addEventListener('start', () => console.log("Controls Start Event"))
-// controls.addEventListener('end', () => console.log("Controls End Event"))
-// controls.enabled = false
-// controls.rotateSpeed = 1.0;
-// controls.zoomSpeed = 1.2;
-// controls.panSpeed = 0.8;
-// controls.keys = [65, 83, 68]
-// controls.noPan = true //default false
-// controls.noRotate = true //default false
-// controls.noZoom = true //default false
-// controls.staticMoving = true //default false
-// controls.maxDistance = 4;
-// controls.minDistance = 2;
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const menuPanel = document.getElementById('menuPanel');
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', function () {
+    controls.lock();
+}, false);
+const controls = new PointerLockControls(camera, renderer.domElement);
+controls.addEventListener('change', () => console.log("Controls Change"));
+controls.addEventListener('lock', () => menuPanel.style.display = 'none');
+controls.addEventListener('unlock', () => menuPanel.style.display = 'block');
+const planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: false });
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.rotateX(-Math.PI / 2);
+scene.add(plane);
+let cubes = new Array();
+for (let i = 0; i < 100; i++) {
+    const geo = new THREE.BoxGeometry(Math.random() * 4, Math.random() * 16, Math.random() * 4);
+    const mat = new THREE.MeshBasicMaterial({ wireframe: false });
+    switch (i % 3) {
+        case 0:
+            mat.color = new THREE.Color(0xff0000);
+            break;
+        case 1:
+            mat.color = new THREE.Color(0xffff00);
+            break;
+        case 2:
+            mat.color = new THREE.Color(0x0000ff);
+            break;
+    }
+    const cube = new THREE.Mesh(geo, mat);
+    cubes.push(cube);
+}
+cubes.forEach((c) => {
+    c.position.x = (Math.random() * 100) - 50;
+    c.position.z = (Math.random() * 100) - 50;
+    c.geometry.computeBoundingBox();
+    c.position.y = (c.geometry.boundingBox.max.y - c.geometry.boundingBox.min.y) / 2;
+    scene.add(c);
+});
+camera.position.y = 1;
 camera.position.z = 2;
+const onKeyDown = function (event) {
+    switch (event.keyCode) {
+        case 87: // w
+            controls.moveForward(.25);
+            break;
+        case 65: // a
+            controls.moveRight(-.25);
+            break;
+        case 83: // s
+            controls.moveForward(-.25);
+            break;
+        case 68: // d
+            controls.moveRight(.25);
+            break;
+    }
+};
+document.addEventListener('keydown', onKeyDown, false);
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -41,7 +77,7 @@ const stats = Stats();
 document.body.appendChild(stats.dom);
 var animate = function () {
     requestAnimationFrame(animate);
-    controls.update();
+    //controls.update()
     render();
     stats.update();
 };
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,12 +1,10 @@
 import * as THREE from '/build/three.module.js'
-import { TrackballControls } from '/jsm/controls/TrackballControls'
+import { PointerLockControls } from '/jsm/controls/PointerLockControls'
 import Stats from '/jsm/libs/stats.module'
 
 const scene: THREE.Scene = new THREE.Scene()
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper)
 
-var light = new THREE.HemisphereLight();
+var light = new THREE.AmbientLight();
 scene.add(light);
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -15,29 +13,71 @@ const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new TrackballControls(camera, renderer.domElement)
-// controls.addEventListener('change', () => console.log("Controls Change"))
-// controls.addEventListener('start', () => console.log("Controls Start Event"))
-// controls.addEventListener('end', () => console.log("Controls End Event"))
-// controls.enabled = false
-// controls.rotateSpeed = 1.0;
-// controls.zoomSpeed = 1.2;
-// controls.panSpeed = 0.8;
-// controls.keys = [65, 83, 68]
-// controls.noPan = true //default false
-// controls.noRotate = true //default false
-// controls.noZoom = true //default false
-// controls.staticMoving = true //default false
-// controls.maxDistance = 4;
-// controls.minDistance = 2;
+const menuPanel = document.getElementById('menuPanel');
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', function () {
+    controls.lock();
+}, false);
 
-const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+const controls = new PointerLockControls(camera, renderer.domElement)
+controls.addEventListener('change', () => console.log("Controls Change"))
+controls.addEventListener('lock', () => menuPanel.style.display = 'none');
+controls.addEventListener('unlock', () => menuPanel.style.display = 'block');
 
-const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
-scene.add(cube)
 
+
+const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(100, 100, 50, 50)
+const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: false })
+const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
+plane.rotateX(-Math.PI / 2)
+scene.add(plane)
+
+let cubes: THREE.Mesh[] = new Array()
+for (let i = 0; i < 100; i++) {
+  const geo: THREE.BoxGeometry = new THREE.BoxGeometry(Math.random() * 4, Math.random() * 16, Math.random() * 4)
+  const mat: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ wireframe: false })
+  switch (i % 3) {
+    case 0:
+      mat.color = new THREE.Color(0xff0000)
+      break;
+    case 1:
+      mat.color = new THREE.Color(0xffff00)
+      break;
+    case 2:
+      mat.color = new THREE.Color(0x0000ff)
+      break;
+  }
+  const cube = new THREE.Mesh(geo, mat)
+  cubes.push(cube)
+}
+cubes.forEach((c) => {
+  c.position.x = (Math.random() * 100) - 50
+  c.position.z = (Math.random() * 100) - 50
+  c.geometry.computeBoundingBox()
+  c.position.y = ((c.geometry.boundingBox as THREE.Box3).max.y - (c.geometry.boundingBox as THREE.Box3).min.y) / 2
+  scene.add(c)
+});
+
+camera.position.y = 1
 camera.position.z = 2
+
+const onKeyDown = function (event) {
+    switch (event.keyCode) {
+        case 87: // w
+            controls.moveForward(.25)
+            break;
+        case 65: // a
+            controls.moveRight(-.25)
+            break;
+        case 83: // s
+            controls.moveForward(-.25)
+            break;
+        case 68: // d
+            controls.moveRight(.25)
+            break;
+    }
+};
+document.addEventListener('keydown', onKeyDown, false);
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -53,7 +93,7 @@ document.body.appendChild(stats.dom)
 var animate = function () {
   requestAnimationFrame(animate)
 
-  controls.update()
+  //controls.update()
 
   render()
 
